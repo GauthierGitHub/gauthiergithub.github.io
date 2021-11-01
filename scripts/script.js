a3b3fbdeb4eaa53c492c21b5a11714e6e9fcb29e
Gauthier Barbet <gauthier.barbet@outlook.fr>
2021-11-01 14:54:38 +0100

    Initial Commit

---
+++ scripts/script.js
@@ -0,0 +1,13 @@
+$("#print-btn").click(function () {
+    var url = "http://ec2-15-188-119-107.eu-west-3.compute.amazonaws.com/cv_html_css/";
+    //api.apiflash.com
+    $.ajax({
+        type: "GET",
+        url: "https://api.apiflash.com/v1/urltoimage?access_key=daf64b4418814bad9cc6cac408e451dd&url=" + url + "&width=2480&height=3508",
+        data: "data",
+        dataType: "dataType",
+        success: function (response) {
+            console.log(response);
+        }
+    });
+});
